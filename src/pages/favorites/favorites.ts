f4b635af609301dfa81d18929109034b3fd52a46
Johan Van Wambeke <johan.van.wambeke@gmail.com>
2018-10-21 18:16:40 +0200

    favorites added

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Quote } from '@angular/compiler';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { QuotesService } from '../../services/quotes';
+import { Quote } from '../../Data/quote.interface';
+import { QuotePage } from '../quote/quote';
 
 
 @Component({
@@ -8,5 +10,19 @@ import { Quote } from '@angular/compiler';
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
+  quotes:Quote[];
+  constructor (
+    private quoteService:QuotesService,
+    private modalController:ModalController){
+    
+  }
+  ionViewWillEnter(){
+    this.quotes = this.quoteService.getFavoriteQuotes(); 
+  }
 
+  onViewQuote(Quote:Quote){
+    //Hier ga ik een modal window open doen om een quote te tonen.
+    const modal = this.modalController.create(QuotePage);
+    modal.present();
+  }
 }
